8718ccbb4657467f906218062b7397cf3d5637ea
Spank_E <dvh28@gmail.com>
2019-06-27 19:58:30 -0700

    broken code about us

---
+++ src/app/Developer.ts
@@ -0,0 +1,21 @@
+import{Developer} from './AboutUs'
+
+
+
+
+export const DEVELOPERS: Developer[]=[
+    {pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR92cvIilNnfQidZBLeca2pfI'+
+    'OURQvDKDEwhkIEMEAE1ZvYAcrWkA', name: 'Gary', about: 'What is there to say'+
+    ' about the sexiest man in California, well he likes to golf but he is so'+
+    ' good he decided it would not be fare for him to compete professionally'+
+    ' so he decided to take up programming to "keep him intertained" A little'+
+    ' known fact about Gary is that the first time he had sex after no fap'+
+    ' November his cum came out with so much force and qunity it carved the'+
+    ' way for what we now call the mississippi river... \n poor girl', age: '23'},
+    {pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbkweYNfZlPBvnvGWnPrdm8L'+
+    'IXwI_y6LJHw3W6tq-i5v0NIBdp', name: 'Spank_E', about: 'Not alot is known about'+
+    ' the programmer known as Spank_E, what little information we have tells'+
+    ' us he started programing to make money to settle his porn addiction'+
+    ' He was last spotted at the corner of E Camelback Rd and N 2nd St.'+
+    '\n...watch out', age: '???'}
+];
