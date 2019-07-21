66757c5785159aa6336ef2a331ec34a84719ec38
Spank_E <dvh28@gmail.com>
2019-07-21 13:02:34 -0700

    dakota3-3

--- src/app/dropdown-v2/dropdown-v2.component.ts
+++ src/app/dropdown-v2/dropdown-v2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DropdownItem } from '../Dropdown_items'
+import { DropdownItem } from '../Dropdown_items';
+import {User} from '../user';
 
 @Component({
   selector: 'app-dropdown-v2',
@@ -8,10 +9,33 @@ import { DropdownItem } from '../Dropdown_items'
 })
 export class DropdownV2Component implements OnInit {
 
+  user = User[0]
+
+  valueArray = [this.user.astValue, this.user.expValue]
+  titleArray = [this.user.astTitle, this.user.expTitle]
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  edit(item1, item2, itemValue){
+    if(itemValue == ''){
+      window.alert("please enter a value")
+    }
+    else{
+      this.valueArray[item1][item2] = itemValue;
+    }
+  }
+
+delete(item1, item2){
+  this.valueArray[item1].splice(item2,1);
+  this.titleArray[item1].splice(item2,1);
+}
+
+Add(item, title, value){
+  this.titleArray[item].push(title)
+  this.valueArray[item].push(value)
+}
   DropDownV2 = DropdownItem
 }
